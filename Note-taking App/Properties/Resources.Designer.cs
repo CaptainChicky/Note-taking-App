462c1d7296e05febf001f16178d4e41623d812d3
CaptainChicky <70708832+CaptainChicky@users.noreply.github.com>
2023-07-12 23:05:11 -0400

    reworked the entire app

--- Note-taking App/Properties/Resources.Designer.cs
+++ Note-taking App/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Note_taking_App.Properties
+namespace NoteTakingApp.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace Note_taking_App.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Note_taking_App.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NoteTakingApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
